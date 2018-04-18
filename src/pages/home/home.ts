cca09143c22cdebb68248760361f1610f297de09
pointdeveloper <pointdeveloper99@gmail.com>
2018-04-18 20:19:17 +0530

    initial comit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, Platform } from "ionic-angular";
+import { NativeAudio } from '@ionic-native/native-audio';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,public platform: Platform,private nativeAudio: NativeAudio) {
+    this.platform.ready().then(() => {
+      this.nativeAudio.preloadSimple('uniqueId1', 'assets/audio/ding.mp3').then((success)=>{
+        console.log("success");
+      },(error)=>{
+        console.log(error);
+      });
+    });
   }
 
+  play(){
+    this.nativeAudio.play('uniqueId1').then((success)=>{
+      console.log("success playing");
+    },(error)=>{
+      console.log(error);
+    });
+  }
 }
